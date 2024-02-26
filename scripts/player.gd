5ca04c3471010e86f86bc2501b1581995191e493
Raul Gonzalez <info@rgonzalez.tech>
2024-02-25 20:56:21 -0800

    minor refactor to Player

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,10 +6,14 @@ extends RigidBody2D
 var input_manager : InputManager
 
 func _ready():
+	_subscribe_to_inputs()
+	add_to_group("player") # Always add the player to the "player" group
+
+# Private methods
+func _subscribe_to_inputs() -> void:
 	input_manager = InputManager.new()
 	input_manager.jumped.connect(_on_jumped)
 	add_child(input_manager)
-	add_to_group("player")
 
 func _on_jumped() -> void:
 	# Jump with a maximum velocity of jump_strength
