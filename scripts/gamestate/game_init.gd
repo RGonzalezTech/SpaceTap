b7c86c6aa99187d7308afe02e7694baa47bba05a
BuildTools <unconfigured@null.spigotmc.org>
2024-04-29 23:14:18 -0700

    Pause the game on Cancel key press

--- scripts/gamestate/game_init.gd
+++ scripts/gamestate/game_init.gd
@@ -9,10 +9,13 @@ signal game_ready
 func _ready():
 	assert(splash_manager, "SplashScreenManager not set in GameStateManager")
 	assert(main_menu, "Main Menu scene required for GameInit")
+
+	GameStateManager.main_menu_scene = main_menu
+
 	splash_manager.all_finished.connect(_on_splash_finish)
 	splash_manager.display_all_splash()
 
 func _on_splash_finish() -> void:
 	game_ready.emit()
 	# Load main menu
-	get_tree().change_scene_to_packed(main_menu)
+	GameStateManager.go_main_menu()
---
+++ scripts/ui/pause_ui.gd
@@ -0,0 +1,31 @@
+class_name PauseUI
+extends BaseUI
+
+@export var resume_btn : Button
+@export var quit_btn : Button
+@export var popup : PopupPanel
+
+func pause_show():
+	get_tree().paused = true
+	popup.popup()
+
+func _ready():
+	assert(resume_btn, "PauseUI: 'resume_btn' is required")
+	assert(quit_btn, "PauseUI: 'quit_btn' is required")
+	assert(popup, "PauseUI: 'popup' is required")
+	
+	_subscribe()
+
+func _subscribe() -> void:
+	resume_btn.pressed.connect(_on_resume)
+	quit_btn.pressed.connect(_on_quit)
+	popup.popup_hide.connect(_on_ui_hide)
+
+func _on_resume() -> void:
+	popup.hide()
+
+func _on_quit() -> void:
+	GameStateManager.go_main_menu()
+
+func _on_ui_hide() -> void:
+	get_tree().paused = false
